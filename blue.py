9ec144ba3d917b32deac74fd00c61206d78c64c2
J. B. Miles <jb_miles@berkeley.edu>
2023-04-20 23:58:40 +0000

    first commit

---
+++ blue.py
@@ -0,0 +1,11 @@
+#is_blue=True
+#is_blue=False
+#color="red"
+color="yellow"
+#if is_blue:
+if color=="blue":
+    print("The object is blue.")
+elif color=="yellow":
+    print("The object is yellow.")
+else:
+    print("The object is not blue.")
